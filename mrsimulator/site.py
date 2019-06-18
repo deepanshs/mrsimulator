f0f04dd557c9630193831f7b743ec808f861d8ee
Shyam Dwaraknath <shyamd@lbl.gov>
2019-06-18 09:10:57 -0700

    add custom parse for sub-dict input

--- mrsimulator/site.py
+++ mrsimulator/site.py
@@ -1,5 +1,5 @@
-from mrsimulator import Parseable
 from typing import ClassVar
+from mrsimulator import Parseable
 
 __author__ = "Deepansh J. Srivastava"
 __email__ = ["srivastava.89@osu.edu", "deepansh2012@gmail.com"]
@@ -30,3 +30,18 @@ class Site(Parseable):
         "beta": "rad",
         "gamma": "rad",
     }
+
+    @classmethod
+    def parse_json_with_units(cls, json_dict):
+
+        if "shielding_symmetric" in json_dict:
+            for k, v in json_dict["shielding_symmetric"].items():
+                json_dict[k] = v
+            del json_dict["shielding_symmetric"]
+
+        if "orientation" in json_dict:
+            for k, v in json_dict["shielding_symmetric"].items():
+                json_dict[k] = v
+            del json_dict["orientation"]
+
+        return super().parse_json_with_units(json_dict)
