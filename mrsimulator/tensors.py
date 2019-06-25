43f663be98ac8f875e100a06d5dfc61e798ec8e0
Shyam Dwaraknath <shyamd@lbl.gov>
2019-06-25 09:34:52 -0700

    Add Tensor

---
+++ mrsimulator/tensors.py
@@ -0,0 +1,47 @@
+from typing import ClassVar
+from mrsimulator import Parseable
+
+__author__ = "Deepansh J. Srivastava"
+__email__ = ["srivastava.89@osu.edu", "deepansh2012@gmail.com"]
+
+
+class SymmetricTensor(Parseable):
+
+    anisotropy: Optional[float]
+    asymmetry: Optional[float]
+    alpha: Optional[float]
+    beta: Optional[float]
+    gamma: Optional[float]
+
+    property_unit_types: ClassVar = {
+        "anisotropy": ["dimensionless", "frequency"],
+        "alpha": "angle",
+        "beta": "angle",
+        "gamma": "angle",
+    }
+
+    property_default_units: ClassVar = {
+        "anisotropy": ["ppm", "Hz"],
+        "alpha": "rad",
+        "beta": "rad",
+        "gamma": "rad",
+    }
+
+
+class AntisymmetricTensor(Parseable):
+
+    anisotropy: Optional[float]
+    alpha: Optional[float]
+    beta: Optional[float]
+
+    property_unit_types: ClassVar = {
+        "anisotropy": ["dimensionless", "frequency"],
+        "alpha": "angle",
+        "beta": "angle",
+    }
+
+    property_default_units: ClassVar = {
+        "anisotropy": ["ppm", "Hz"],
+        "alpha": "rad",
+        "beta": "rad",
+    }
