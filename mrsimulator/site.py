6dad2271125e88205811035e792ebf0bd80c6898
Shyam Dwaraknath <shyamd@lbl.gov>
2019-06-25 09:35:16 -0700

    update site schema

--- mrsimulator/site.py
+++ mrsimulator/site.py
@@ -1,5 +1,5 @@
-from typing import ClassVar
-from mrsimulator import Parseable
+from typing import ClassVar, Optional
+from mrsimulator import Parseable, SymmetricTensor, AntisymmetricTensor
 
 __author__ = "Deepansh J. Srivastava"
 __email__ = ["srivastava.89@osu.edu", "deepansh2012@gmail.com"]
@@ -7,13 +7,11 @@ __email__ = ["srivastava.89@osu.edu", "deepansh2012@gmail.com"]
 
 class Site(Parseable):
 
-    nucleus: str = "1H"
-    isotropic_chemical_shift: float = 0
-    anisotropy: float = 0
-    asymmetry: float = 0
-    alpha: float = 0
-    beta: float = 0
-    gamma: float = 0
+    isotope: str = "1H"
+    isotropic_chemical_shift: Optional[float]
+    shielding_symmetric: Optional[SymmetricTensor]
+    shielding_antisymmetric: Optional[AntisymmetricTensor]
+    quadrupolar: Optional[SymmetricTensor]
 
     property_unit_types: ClassVar = {
         "isotropic_chemical_shift": ["dimensionless","frequency"],
@@ -25,27 +23,11 @@ class Site(Parseable):
 
     property_default_units: ClassVar = {
         "isotropic_chemical_shift": ["ppm","Hz"],
-        "anisotropy": ["ppm","Hz"],
-        "alpha": "rad",
-        "beta": "rad",
-        "gamma": "rad",
     }
 
     @classmethod
     def parse_json_with_units(cls, json_dict):
 
-        if "isotope_symbol" in json_dict:
-            json_dict["nucleus"] = json_dict["isotope_symbol"]
-            del json_dict["isotope_symbol"]
 
-        if "shielding_symmetric" in json_dict:
-            for k, v in json_dict["shielding_symmetric"].items():
-                json_dict[k] = v
-            del json_dict["shielding_symmetric"]
-
-        if "orientation" in json_dict:
-            for k, v in json_dict["shielding_symmetric"].items():
-                json_dict[k] = v
-            del json_dict["orientation"]
 
         return super().parse_json_with_units(json_dict)
