c45bb67b1facb9eabab8138c2ab5b742f185b970
Deepansh Srivastava <deepansh2012@gmail.com>
2019-10-19 23:25:06 -0400

    .

--- build_wheel.sh
+++ build_wheel.sh
@@ -3,24 +3,24 @@ set -e -x
 
 # Install a system package required by our library
 yum install -y openblas-devel git fftw-devel
-mkdir mrsim
-git clone --single-branch --branch master https://github.com/DeepanshS/mrsimulator.git mrsim
-cd mrsim
+mkdir io
+git clone --single-branch --branch master https://github.com/DeepanshS/mrsimulator.git io
+cd io
 
 # Compile wheels
 for PYBIN in /opt/python/cp3[6-8]*/bin; do
     "${PYBIN}/pip" install -r requirements.txt
-    "${PYBIN}/python" setup.py bdist_wheel --dist-dir=/mrsim/dist/
+    "${PYBIN}/python" setup_unix.py bdist_wheel --dist-dir=/io/dist/
 done
 
 # Bundle external shared libraries into the wheels
 for whl in dist/*.whl; do
-    auditwheel repair "$whl" -w /mrsim/dist/
+    auditwheel repair "$whl" -w /io/dist/
 done
 
 
 # Install packages and test
 # for PYBIN in /opt/python/*/bin/; do
-#     "${PYBIN}/pip" install python-manylinux-demo --no-index -f /mrsim/dist
+#     "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/dist
 #     (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
 # done
