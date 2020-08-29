1d9b8b545379654daad639f08e19cccc3e6fa656
Deepansh Srivastava <deepansh2012@gmail.com>
2020-08-29 15:22:52 -0400

    update logo subtitle update czjzek example docs update 1D examples readme

--- examples/1D_simulation/plot_5_czjzek_distribution.py
+++ examples/1D_simulation/plot_5_czjzek_distribution.py
@@ -60,9 +60,7 @@ plt.show()
 # :math:`\eta` parameters, use the
 # :func:`~mrsimulator.utils.collection.single_site_system_generator` utility function.
 systems = single_site_system_generator(
-    isotopes="13C",
-    shielding_symmetric={"zeta": z_dist.ravel(), "eta": e_dist.ravel()},
-    abundance=amp.ravel(),
+    isotopes="13C", shielding_symmetric={"zeta": z_dist, "eta": e_dist}, abundance=amp,
 )
 
 # %%
@@ -116,9 +114,7 @@ plt.show()
 #
 # Create the spin systems.
 systems = single_site_system_generator(
-    isotopes="71Ga",
-    quadrupolar={"Cq": cq_dist.ravel() * 1e6, "eta": e_dist.ravel()},
-    abundance=amp.ravel(),
+    isotopes="71Ga", quadrupolar={"Cq": cq_dist * 1e6, "eta": e_dist}, abundance=amp,
 )
 
 # %%
