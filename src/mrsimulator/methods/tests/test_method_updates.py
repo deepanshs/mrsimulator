e6684659ab4cb3dc808c93b65b45b29fc110e0e6
Deepansh Srivastava <deepansh2012@gmail.com>
2021-03-21 19:41:08 -0400

    fixed codefactor issues.

--- src/mrsimulator/methods/tests/test_method_updates.py
+++ src/mrsimulator/methods/tests/test_method_updates.py
@@ -19,6 +19,42 @@ sys = SpinSystem(sites=[{"isotope": "1H"}])
 
 
 def test_read_write_methods():
+    def assert_parsing(method, fn1):
+        fn2 = method.parse_dict_with_units(fn1.json())
+        assert fn1 == fn2, f"Error with {method} parse with units."
+
+        fn3 = method(**fn1.json(unit=False))
+        assert fn1 == fn3, f"Error with {method} parse with units."
+
+        event_error = "Event objects are immutable for"
+        serialize = fn1.json()
+        ent = serialize["spectral_dimensions"][0]["events"]
+        ent[0]["transition_query"][0]["ch1"]["P"] = [-100]
+
+        if method not in [Method, Method1D, Method2D]:
+            with pytest.raises(ImmutableEventError, match=f".*{event_error}.*"):
+                method.parse_dict_with_units(serialize)
+
+            with pytest.raises(ImmutableEventError, match=f".*{event_error}.*"):
+                method(**serialize)
+
+            with pytest.raises(ImmutableEventError, match=f".*{event_error}.*"):
+                ent.append({"transition_query": [{"ch1": {"P": [-100]}}]})
+                method.parse_dict_with_units(serialize)
+
+    def check_sim_save(sim1, sim2):
+        for mth in sim2.methods:
+            mth.simulation._timestamp = ""
+            _ = [item.to("ppm", "nmr_frequency_ratio") for item in mth.simulation.x]
+
+        data1 = sim.methods[0].simulation.copy()
+        sim.methods[0].simulation = None
+        data2 = sim2.methods[0].simulation.copy()
+        sim2.methods[0].simulation = None
+
+        assert data1 == data2, f"data saved and loaded is not equal: method {item}."
+        assert sim == sim2, f"Error in method {item} when using Simulator.load()."
+
     for item in methods:
         kwargs = {
             "channels": ["93Nb"],
@@ -32,54 +68,17 @@ def test_read_write_methods():
 
         fn1 = item(**kwargs)
 
-        def assert_parsing(method, this=False):
-            fn2 = method.parse_dict_with_units(fn1.json())
-            assert fn1 == fn2, f"Error with {method} parse with units."
-
-            fn3 = method(**fn1.json(unit=False))
-            assert fn1 == fn3, f"Error with {method} parse with units."
-
-            event_error = "Event objects are immutable for"
-            serialize = fn1.json()
-            ent = serialize["spectral_dimensions"][0]["events"]
-            ent[0]["transition_query"][0]["ch1"]["P"] = [-100]
-
-            if method not in [Method, Method1D, Method2D]:
-                print("method name", method.__name__)
-                with pytest.raises(ImmutableEventError, match=f".*{event_error}.*"):
-                    method.parse_dict_with_units(serialize)
-
-                with pytest.raises(ImmutableEventError, match=f".*{event_error}.*"):
-                    method(**serialize)
-
-                with pytest.raises(ImmutableEventError, match=f".*{event_error}.*"):
-                    ent.append({"transition_query": [{"ch1": {"P": [-100]}}]})
-                    method.parse_dict_with_units(serialize)
-
         # Parse against self
-        assert_parsing(item)
+        assert_parsing(item, fn1)
 
         # Parse against Method class
-        assert_parsing(Method)
+        assert_parsing(Method, fn1)
 
         # Parse against Method1D/Method2D classes
         mth_d = Method1D if item.ndim == 1 else Method2D
-        assert_parsing(mth_d)
+        assert_parsing(mth_d, fn1)
 
         # save test
-        def check_sim_save(sim1, sim2):
-            for mth in sim2.methods:
-                mth.simulation._timestamp = ""
-                _ = [item.to("ppm", "nmr_frequency_ratio") for item in mth.simulation.x]
-
-            data1 = sim.methods[0].simulation.copy()
-            sim.methods[0].simulation = None
-            data2 = sim2.methods[0].simulation.copy()
-            sim2.methods[0].simulation = None
-
-            assert data1 == data2, f"data saved and loaded is not equal: method {item}."
-            assert sim == sim2, f"Error in method {item} when using Simulator.load()."
-
         sim = Simulator(spin_systems=[sys], methods=[fn1])
 
         # save/load with units
