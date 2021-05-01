bed7a9a22e9cd248cbea9a873e3e8a05464ace50
Deepansh Srivastava <deepansh2012@gmail.com>
2021-05-01 03:36:57 -0400

    improve code coverage.

--- src/mrsimulator/models/czjzek.py
+++ src/mrsimulator/models/czjzek.py
@@ -69,9 +69,6 @@ def _czjzek_random_distribution_tensors(sigma, n):
 
 
 class AbstractDistribution:
-    def rvs(self):
-        pass
-
     def pdf(self, pos, size: int = 400000):
         """Generates a probability distribution function by binning the random
         variates of length size onto the given grid system.
