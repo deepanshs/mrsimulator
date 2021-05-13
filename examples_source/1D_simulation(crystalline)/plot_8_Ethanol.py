706784716eaeff17f7bd312e6047976f136a51a2
Deepansh Srivastava <deepansh2012@gmail.com>
2021-05-13 11:02:59 -0400

    Fix formatting. Add links.

--- examples_source/1D_simulation(crystalline)/plot_8_Ethanol.py
+++ examples_source/1D_simulation(crystalline)/plot_8_Ethanol.py
@@ -1,37 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Ethanol Revisited
-^^^^^^^^^^^^^^^^^
+Ethanol Revisited (1H and 13C NMR)
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
+Simulating 1H and 13C isotopomers.
 """
 # %%
-#
-#
-# An astute observer may have noticed that the :math:`^{1}\text{H}` ethanol
-# spectrum from **The Basics: Coupled Spin System** was missing the
-# characteristic :math:`^{13}C` satellite peaks.  In this example, we will add
-# these to the :math:`^1H` spectrum and also plot the :math:`^{13}C` spectrum
-# while we're at it!
+# An astute observer may have noticed that the :math:`^{1}\text{H}` ethanol spectrum
+# from :ref:`getting_started_coupled_spin_system_etoh` was missing the characteristic
+# :math:`^{13}\text{C}`
+# `satellite peaks <https://en.wikipedia.org/wiki/Carbon-13_NMR_satellite>`_.
+# In this example, we will add these to the :math:`^1\text{H}` spectrum and also plot
+# the :math:`^{13}\text{C}` spectrum while we're at it!
 #
 # We'll start importing the necessary packages, just like before.
-
-# %%
 import matplotlib.pyplot as plt
+
 from mrsimulator import Simulator, SpinSystem, Site, Coupling
 from mrsimulator.methods import BlochDecaySpectrum
-import mrsimulator.signal_processing as sp
-import mrsimulator.signal_processing.apodization as apo
+from mrsimulator import signal_processing as sp
+
+# sphinx_gallery_thumbnail_number = 1
 
 # %%
 # **Spin Systems**
 #
 # The satellite peaks come from low-abundance isotopomers that have one
-# :math:`^{13}\text{C}`` in them, causing more splittings.  First, let's define
-# all the possible :math:`^1H` and :math:`^{13}C` sites.
-
-# %%
-
+# :math:`^{13}\text{C}` in them, causing more splittings. First, let's define
+# all the possible :math:`^1\text{H}` and :math:`^{13}\text{C}` sites.
 
 H_CH3 = Site(isotope="1H", isotropic_chemical_shift=1.226)
 H_CH2 = Site(isotope="1H", isotropic_chemical_shift=2.61)
@@ -41,14 +38,16 @@ C_CH3 = Site(isotope="13C", isotropic_chemical_shift=18)
 C_CH2 = Site(isotope="13C", isotropic_chemical_shift=58)
 
 # %%
-# Now, let's define the couplings and build the spin system for the most
-# abundant isotopomer (pictured below).
+# Now, let's define the couplings and build the spin system for the most abundant
+# isotopomer (pictured below).
 #
-# .. image::  ../../_static/iso1.*
+# .. figure::  ../../_static/iso1.*
 #     :width: 200
 #     :alt: figure
-
-# %%
+#     :align: center
+#
+#     An isotopomer of ethanol containing all :math:`^{1}\text{H}` and all
+#     :math:`^{12}\text{C}` isotopes.
 
 iso1_sites = [H_CH3, H_CH3, H_CH3, H_CH2, H_CH2, H_OH]
 
@@ -71,23 +70,23 @@ iso1_couplings = [
 iso1 = SpinSystem(sites=iso1_sites, couplings=iso1_couplings, abundance=97.812)
 
 # %%
-# A note about abundance: these values were calculated using basic rules of
-# probability and an assumption that only :math:`^1H` and :math:`^{16}O` are
-# present. The abundance of :math:`^{12}C` is 98.9% and the abundance of
-# :math:`^{13}C` is 1.1%. So, the probablity of the most abundant isotopomer
+# A note about abundance: these values were calculated using basic rules of probability
+# and an assumption that only :math:`^1\text{H}` and :math:`^{16}\text{O}` are
+# present. The abundance of :math:`^{12}\text{C}` is 98.9% and the abundance of
+# :math:`^{13}\text{C}` is 1.1%. So, the probability of the most abundant isotopomer
 # is :math:`0.989*0.989=0.97812`
 #
-# Now, we build the sites, couplings (:math:`^1J_{CH}` and :math:`^3J_{HH}`),
+# Now, we build the sites, couplings (:math:`^1J_\text{CH}` and :math:`^3J_\text{HH}`),
 # and spin system for the isotopomer with the methyl carbon replaced with a
-# :math:`^{13}C` (pictured below, :math:`^{13}C` marked in blue)
+# :math:`^{13}\text{C}` (pictured below, :math:`^{13}\text{C}` marked in blue)
 #
-#
-# .. image::  ../../_static/iso2.*
+# .. figure::  ../../_static/iso2.*
 #     :width: 200
 #     :alt: figure
-
-# %%
-
+#     :align: center
+#
+#     Second isotopomer of ethanol containing all :math:`^{1}\text{H}`,
+#     :math:`^{13}\text{C}` methyl, and :math:`^{12}\text{C}` methylene isotopes.
 
 iso2_sites = [H_CH3, H_CH3, H_CH3, H_CH2, H_CH2, H_OH, C_CH3]
 
@@ -100,17 +99,17 @@ iso2_couplings = iso1_couplings + [CH3_coupling_1, CH3_coupling_2, CH3_coupling_
 iso2 = SpinSystem(sites=iso2_sites, couplings=iso2_couplings, abundance=1.088)
 
 # %%
-#
 # Lastly, we build the sites, couplings, and spin system for the other
-# isotopomer with the methylene carbon replaced with :math:`^{13}C` (pictured
-# below, :math:`^{13}C` marked in blue)
+# isotopomer with the methylene carbon replaced with :math:`^{13}\text{C}` (pictured
+# below, :math:`^{13}\text{C}` marked in blue)
 #
-# .. image::  ../../_static/iso3.*
+# .. figure::  ../../_static/iso3.*
 #     :width: 200
 #     :alt: figure
-
-# %%
-
+#     :align: center
+#
+#     Third isotopomer of ethanol containing all :math:`^{1}\text{H}`,
+#     :math:`^{12}\text{C}` methyl, and :math:`^{13}\text{C}` methylene isotopes.
 
 iso3_sites = [H_CH3, H_CH3, H_CH3, H_CH2, H_CH2, H_OH, C_CH2]
 
@@ -122,104 +121,88 @@ iso3_couplings = iso1_couplings + [CH2_coupling_1, CH2_coupling_2]
 iso3 = SpinSystem(sites=iso3_sites, couplings=iso3_couplings, abundance=1.085)
 
 # %%
-#
 # **Methods**
 #
-# Now, we define simple 1 pulse-acquire methods for both :math:`^1H` and
-# :math:`^{13}C`.
-
-# %%
-
-
+# Now, we define simple 1 pulse-acquire methods for both :math:`^1\text{H}` and
+# :math:`^{13}\text{C}`.
 method_H = BlochDecaySpectrum(
     channels=["1H"],
     magnetic_flux_density=9.4,  # T
     spectral_dimensions=[
-        {"count": 16000, "spectral_width": 1.5e3, "reference_offset": 950}  # in Hz
+        {
+            "count": 16000,
+            "spectral_width": 1.5e3,
+            "reference_offset": 950,
+            "label": "$^{1}$H frequency",
+        }
     ],
-)  # in Hz
+)
 
 method_C = BlochDecaySpectrum(
     channels=["13C"],
     magnetic_flux_density=9.4,  # T
     spectral_dimensions=[
-        {"count": 32000, "spectral_width": 8e3, "reference_offset": 4e3}
+        {
+            "count": 32000,
+            "spectral_width": 8e3,
+            "reference_offset": 4e3,
+            "label": "$^{13}$C frequency",
+        }
     ],
 )
 
 # %%
-#
 # **Simulation**
 #
 # Now, we create an instance of the simulator object, add our three spin
 # systems, add our two methods, and run the simulation.
-
-# %%
-
-
-sim = Simulator()
-sim.spin_systems = [iso1, iso2, iso3]
-sim.methods = [method_H, method_C]
+sim = Simulator(spin_systems=[iso1, iso2, iso3], methods=[method_H, method_C])
 sim.run()
 
 # %%
 # Let's set up our post-simulation processing.
-
-# %%
-
-
-processor = sp.SignalProcessor(
+processor_1H = sp.SignalProcessor(
     operations=[
         sp.IFFT(),
-        apo.Exponential(FWHM="1 Hz"),
+        sp.apodization.Exponential(FWHM="1 Hz"),
+        sp.FFT(),
+    ]
+)
+
+processor_13C = sp.SignalProcessor(
+    operations=[
+        sp.IFFT(),
+        sp.apodization.Exponential(FWHM="20 Hz"),
         sp.FFT(),
     ]
 )
 
 # %%
 # Now, let's get our two datasets out of the simulation object and apply the
-# post-processing
-
-# %%
-
-
+# post-processing.
 H_data = sim.methods[0].simulation
 C_data = sim.methods[1].simulation
 
-processed_H_data = processor.apply_operations(data=H_data)
-processed_C_data = processor.apply_operations(data=C_data)
+processed_H_data = processor_1H.apply_operations(data=H_data)
+processed_C_data = processor_13C.apply_operations(data=C_data)
 
 # %%
 # Lastly, we plot the two spectra!
-
-# %%
-
-
 fig, ax = plt.subplots(
-    nrows=1, ncols=2, subplot_kw={"projection": "csdm"}, figsize=[8, 4]
+    nrows=1, ncols=2, subplot_kw={"projection": "csdm"}, figsize=[8, 3.5]
 )
 
-ax[0].plot(
-    processed_H_data.real,
-    color="black",
-    linewidth=0.5,
-)
+ax[0].plot(processed_H_data.real, color="black", linewidth=0.5)
 ax[0].invert_xaxis()
-ax[0].set_title("$^1H$")
+ax[0].set_title("$^1$H")
 
-ax[1].plot(
-    processed_C_data.real,
-    color="black",
-    linewidth=1,
-)
+ax[1].plot(processed_C_data.real, color="black", linewidth=1)
 ax[1].invert_xaxis()
-ax[1].set_title("$^{13}C$")
+ax[1].set_title("$^{13}$C")
 
 plt.tight_layout()
 plt.show()
 
 # %%
-#
-#
 # Now, we see the :math:`^{13}C` satellites on either side of the peaks near 1.2
-# ppm and 2.6 ppm in the :math:`^1H` spectrum.
+# ppm and 2.6 ppm in the :math:`^1\text{H}` spectrum.
