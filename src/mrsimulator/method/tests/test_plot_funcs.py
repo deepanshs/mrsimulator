ff13215346822f5b15bd36ea34405611211e4576
Matthew Giammar <mdgiammar@gmail.com>
2021-06-17 12:37:04 -0400

    Added test file for plotting tests

---
+++ src/mrsimulator/method/tests/test_plot_funcs.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+from mrsimulator.method import Method
+
+# import pytest
+
+__author__ = "Matthew D. Giammar"
+__email__ = "giammar.7@buckeyemail.osu.edu"
+
+
+# TODO test _make_x_data
+# TODO test _offset_x_data
+# TODO test _add_tip_angle_and_phase
+# TODO test plot exits (df)
