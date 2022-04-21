1cbc7b034a082616009d25ee857e07d74dd9f78c
Matthew Giammar <mdgiammar@gmail.com>
2022-04-21 09:21:26 -0400

    Comment out array generation code

--- tests/wigner/elements/wigner_d_elements_test.py
+++ tests/wigner/elements/wigner_d_elements_test.py
@@ -81,28 +81,28 @@ def test_wigner_l_nine_half_elements():
 # This code was used to generate and store wigner d-elements from sympy rotation.
 # All tests are compared with these stored values to speed up the test runs.
 #
-if __name__ == "__main__":
-    from sympy import Integer
-    from sympy.physics.quantum.spin import Rotation
+# if __name__ == "__main__":
+#     from sympy import Integer
+#     from sympy.physics.quantum.spin import Rotation
 
-    beta = np.asarray(ANGLES) * np.pi / 180.0
+#     beta = np.asarray(ANGLES) * np.pi / 180.0
 
-    def prep_setup(l, filename):
-        n1 = int(2 * l + 1)
-        s = [
-            complex(Rotation.d(l, -l + i, -l + j, b).doit()).real
-            for b in beta
-            for i in range(n1)
-            for j in range(n1)
-        ]
-        np.save(filename, s)
+#     def prep_setup(l, filename):
+#         n1 = int(2 * l + 1)
+#         s = [
+#             complex(Rotation.d(l, -l + i, -l + j, b).doit()).real
+#             for b in beta
+#             for i in range(n1)
+#             for j in range(n1)
+#         ]
+#         np.save(filename, s)
 
-    prep_setup(1 / Integer(2), "tests/wigner/elements/2l=1.npy")
-    prep_setup(1, "tests/wigner/elements/2l=2.npy")
-    prep_setup(3 / Integer(2), "tests/wigner/elements/2l=3.npy")
-    prep_setup(2, "tests/wigner/elements/2l=4.npy")
-    prep_setup(5 / Integer(2), "tests/wigner/elements/2l=5.npy")
-    prep_setup(3, "tests/wigner/elements/2l=6.npy")
-    prep_setup(7 / Integer(2), "tests/wigner/elements/2l=7.npy")
-    prep_setup(4, "tests/wigner/elements/2l=8.npy")
-    prep_setup(9 / Integer(2), "tests/wigner/elements/2l=9.npy")
+#     prep_setup(1 / Integer(2), "tests/wigner/elements/2l=1.npy")
+#     prep_setup(1, "tests/wigner/elements/2l=2.npy")
+#     prep_setup(3 / Integer(2), "tests/wigner/elements/2l=3.npy")
+#     prep_setup(2, "tests/wigner/elements/2l=4.npy")
+#     prep_setup(5 / Integer(2), "tests/wigner/elements/2l=5.npy")
+#     prep_setup(3, "tests/wigner/elements/2l=6.npy")
+#     prep_setup(7 / Integer(2), "tests/wigner/elements/2l=7.npy")
+#     prep_setup(4, "tests/wigner/elements/2l=8.npy")
+#     prep_setup(9 / Integer(2), "tests/wigner/elements/2l=9.npy")
