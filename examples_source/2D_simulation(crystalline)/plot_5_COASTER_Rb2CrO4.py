faf9a97f132614e94bc800fdcf6c87f385105c06
Deepansh Srivastava <deepansh2012@gmail.com>
2022-05-30 11:23:46 -0400

    Update plot_5_COASTER_Rb2CrO4.py

--- examples_source/2D_simulation(crystalline)/plot_5_COASTER_Rb2CrO4.py
+++ examples_source/2D_simulation(crystalline)/plot_5_COASTER_Rb2CrO4.py
@@ -42,10 +42,10 @@ spin_system = SpinSystem(sites=[site])
 # Use the generic method, `Method`, to simulate a 2D COASTER spectrum by customizing
 # the method parameters, as shown below.
 #
-# By default, all transitions selected from one `SpectralEvent` will connect to all
-# transitions selected by another `SpectralEvent` if there is no `MixingEvent` between
-# defined them. Here we define a `MixingEvent` with an angle of 109.5 degrees to
-# selectively connect the three quantum and 1 quantum coherences.
+# By default, all transitions selected from a `SpectralEvent` connect to all selected
+# transitions from the following `SpectralEvent` if no `MixingEvent` is defined
+# between them. Here, we define a `MixingEvent` with an angle of 109.5 degrees to
+# connect the 3Q to 1Q transitions.
 coaster = Method(
     name="COASTER",
     channels=["87Rb"],
