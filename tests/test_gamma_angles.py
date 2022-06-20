77dc3e385912a3edcb5a53ae15c32897cc50492e
Deepansh Srivastava <deepansh2012@gmail.com>
2022-06-20 13:50:50 -0400

    gamma avg test for 2D spectrum

--- tests/test_gamma_angles.py
+++ tests/test_gamma_angles.py
@@ -4,6 +4,10 @@ import numpy as np
 from mrsimulator import Simulator
 from mrsimulator import Site
 from mrsimulator import SpinSystem
+from mrsimulator.method import Method
+from mrsimulator.method import MixingEvent
+from mrsimulator.method import SpectralDimension
+from mrsimulator.method import SpectralEvent
 from mrsimulator.method.lib import BlochDecaySpectrum
 
 # import matplotlib.pyplot as plt
@@ -52,3 +56,69 @@ def test_quad_01():
     )
     spin_system = SpinSystem(sites=[site])
     setup_test(spin_system, volume="hemisphere", sw=1e6)
+
+
+def test_2D():
+    site_Ni = Site(
+        isotope="2H",
+        isotropic_chemical_shift=-97,  # in ppm
+        shielding_symmetric=dict(
+            zeta=-551,
+            eta=0.12,
+            alpha=62 * np.pi / 180,
+            beta=114 * np.pi / 180,
+            gamma=171 * np.pi / 180,
+        ),
+        quadrupolar=dict(Cq=77.2e3, eta=0.9),  # Cq in Hz
+    )
+    spin_system = SpinSystem(sites=[site_Ni])
+
+    data = []
+    for angle, n_gamma in zip([0, np.pi / 2], [1, 500]):
+        shifting_d = Method(
+            name="Shifting-d",
+            channels=["2H"],
+            magnetic_flux_density=9.395,  # in T
+            rotor_frequency=0,  # in Hz
+            rotor_angle=angle,  # in Hz
+            spectral_dimensions=[
+                SpectralDimension(
+                    count=512,
+                    spectral_width=2.5e5,  # in Hz
+                    label="Quadrupolar frequency",
+                    events=[
+                        SpectralEvent(
+                            transition_query=[{"ch1": {"P": [-1]}}],
+                            freq_contrib=["Quad1_2"],
+                        ),
+                        MixingEvent(query="NoMixing"),
+                    ],
+                ),
+                SpectralDimension(
+                    count=256,
+                    spectral_width=2e5,  # in Hz
+                    reference_offset=2e4,  # in Hz
+                    label="Paramagnetic shift",
+                    events=[
+                        SpectralEvent(
+                            transition_query=[{"ch1": {"P": [-1]}}],
+                            freq_contrib=["Shielding1_0", "Shielding1_2"],
+                        )
+                    ],
+                ),
+            ],
+        )
+
+        sim = Simulator(spin_systems=[spin_system], methods=[shifting_d])
+        sim.config.integration_volume = "hemisphere"
+        sim.config.number_of_gamma_angles = n_gamma
+        sim.run(auto_switch=(n_gamma == 1))
+
+        data.append(sim.methods[0].simulation.y[0].components[0])
+
+    # _, ax = plt.subplots(1, 2)
+    # ax[0].imshow(data[0].real)
+    # ax[1].imshow(data[1].real)
+    # plt.show()
+
+    np.testing.assert_almost_equal(data[0], data[1], decimal=2)
