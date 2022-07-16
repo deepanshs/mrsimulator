3c9202560c6adbd01bda7aa72808182edf3a4422
Deepansh Srivastava <deepansh2012@gmail.com>
2022-07-15 20:29:19 -0400

    fix transition_query -> transition_queries

--- tests/test_gamma_angles.py
+++ tests/test_gamma_angles.py
@@ -87,7 +87,7 @@ def test_2D():
                     label="Quadrupolar frequency",
                     events=[
                         SpectralEvent(
-                            transition_query=[{"ch1": {"P": [-1]}}],
+                            transition_queries=[{"ch1": {"P": [-1]}}],
                             freq_contrib=["Quad1_2"],
                         ),
                         MixingEvent(query="NoMixing"),
@@ -100,7 +100,7 @@ def test_2D():
                     label="Paramagnetic shift",
                     events=[
                         SpectralEvent(
-                            transition_query=[{"ch1": {"P": [-1]}}],
+                            transition_queries=[{"ch1": {"P": [-1]}}],
                             freq_contrib=["Shielding1_0", "Shielding1_2"],
                         )
                     ],
