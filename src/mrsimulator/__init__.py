8e4058a536208ff7547e9b3d462190f4ef6f6c78
Matthew Giammar <43814525+mgiammar@users.noreply.github.com>
2022-07-19 16:02:10 -0500

    Update version number to 0.7.0

--- src/mrsimulator/__init__.py
+++ src/mrsimulator/__init__.py
@@ -23,7 +23,7 @@ __credits__ = ["Deepansh J. Srivastava"]
 __license__ = "BSD License"
 __maintainer__ = "Deepansh J. Srivastava"
 __status__ = "Beta"
-__version__ = "0.7.0rc2"
+__version__ = "0.7.0"
 
 import os
 
