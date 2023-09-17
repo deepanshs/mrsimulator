8ea8e7230efc8dc7918857316f047acbffe55103
deepanshs <21365911+deepanshs@users.noreply.github.com>
2023-09-17 19:26:08 -0400

    support for python 3.12

--- setup.py
+++ setup.py
@@ -454,6 +454,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Education",
         "Topic :: Scientific/Engineering :: Chemistry",
     ],
