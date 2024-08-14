be627626e2a26b7fc126356434fa1a29daea98c4
deepanshs <21365911+deepanshs@users.noreply.github.com>
2024-08-14 07:46:58 -0400

    setup test

--- src/mrsimulator/__init__.py
+++ src/mrsimulator/__init__.py
@@ -23,7 +23,7 @@ __credits__ = ["Deepansh J. Srivastava"]
 __license__ = "BSD License"
 __maintainer__ = "Deepansh J. Srivastava"
 __status__ = "Beta"
-__version__ = "0.8.0rc1"
+__version__ = "1.0rc1"
 
 import os
 
