5a46dd5954dbd0c9a1ac594287ca024768e9ba42
Philip J. Grandinetti <43141014+pjgrandinetti@users.noreply.github.com>
2024-09-01 08:06:23 -0400

    test_das update but still failing

--- tests/2D_spectrum_tests/test_das.py
+++ tests/2D_spectrum_tests/test_das.py
@@ -7,7 +7,6 @@ from mrsimulator.method import Method
 from mrsimulator.method.lib import BlochDecayCTSpectrum
 from mrsimulator.spin_system.isotope import Isotope
 
-
 # default unit of isotropic_chemical_shift is ppm and Cq is Hz.
 O17 = Isotope(symbol="17O")
 O17_1 = Site(
@@ -40,6 +39,16 @@ O17_5 = Site(
 sites = [O17_1, O17_2, O17_3, O17_4, O17_5]
 abundance = [1, 1, 2, 2, 2]
 spin_systems = [SpinSystem(sites=[s], abundance=a) for s, a in zip(sites, abundance)]
+
+
+def quad_iso_shift(m, C_q, eta_q, spin, delta_iso_cs, nu_0, nu_ref):
+    nu_q = (3 * C_q) / (2 * spin * (2 * spin - 1))
+    nu_ref = nu_ref * (-nu_0 / abs(nu_0))  # nu_ref has opposite sign to nu_0
+    term1 = 2 * m * delta_iso_cs * (-nu_0 * 1e-6)
+    term2 = (2 * m / 30) * ((eta_q**2 / 3) + 1) * (spin * (spin + 1) - 3 * m**2)
+    shift = term1 + (nu_q**2 / nu_0) * term2
+    # divide shift by nu_ref before returning to convert from Hz to ppm
+    return shift
 
 
 def test_DAS():
@@ -92,7 +101,8 @@ def test_DAS():
     sim.run(pack_as_csdm=False)
 
     dataset_das = sim.methods[0].simulation
-    dataset_das_coords_ppm = das.spectral_dimensions[0].coordinates_ppm()
+    dataset_das_coords_Hz = das.spectral_dimensions[0].coordinates_Hz()
+    # dataset_das_coords_ppm = das.spectral_dimensions[0].coordinates_ppm()
 
     # Bloch decay central transition method
     bloch = BlochDecayCTSpectrum(
@@ -122,19 +132,27 @@ def test_DAS():
     spin = das.channels[0].spin
     for i, sys in enumerate(spin_systems):
         for site in sys.sites:
-            Cq = site.quadrupolar.Cq
-            eta = site.quadrupolar.eta
+            Cq = 0
+            eta = 0
+            if site.quadrupolar is not None:
+                Cq = site.quadrupolar.Cq
+                eta = site.quadrupolar.eta
             iso = site.isotropic_chemical_shift
             factor1 = -(3 / 40) * (Cq / larmor_freq) ** 2
             factor2 = (spin * (spin + 1) - 3 / 4) / (spin**2 * (2 * spin - 1) ** 2)
             factor3 = 1 + (eta**2) / 3
             iso_obs = factor1 * factor2 * factor3 * 1e6 + iso
+            # iso_obs = quad_iso_shift(
+            #     0.5, Cq, eta, spin, iso, larmor_freq, O17.B0_to_ref_freq(B0)
+            # )
 
             # get the index where there is a signal
             id1 = dataset_das[i] / dataset_das[i].max()
             index = np.where(id1 == id1.max())[0]
-            iso_spectrum = dataset_das_coords_ppm[index[0]]  # x[1].coords[index[0]]
+            iso_spectrum = dataset_das_coords_Hz[index[0]]  # x[1].coords[index[0]]
+            # iso_spectrum = dataset_das_coords_ppm[index[0]]  # x[1].coords[index[0]]
 
+            print(iso_obs, iso_spectrum)
             # test for the position of isotropic peaks.
             np.testing.assert_almost_equal(iso_obs, iso_spectrum, decimal=1)
 
